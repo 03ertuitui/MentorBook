438ea9d7b44f3ab7ca724adede51e3e9cb1d0935
AkariIto <stella4u0707@gmail.com>
2022-08-26 14:37:27 +0900

    Initial Commit

---
+++ MentorBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentorBook
+//
+//  Created by 伊藤あかり on 2022/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
